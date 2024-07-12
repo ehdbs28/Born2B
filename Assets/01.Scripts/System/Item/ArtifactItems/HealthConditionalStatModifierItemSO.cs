9b0ae5de2eafa1896bc6c85e50974a2ac03841f1
SEH00N <06psh0501@gmail.com>
2024-07-13 07:21:37 +0900

    artifact items

--- Assets/01.Scripts/System/Item/ArtifactItems/HealthConditionalStatModifierItemSO.cs
+++ Assets/01.Scripts/System/Item/ArtifactItems/HealthConditionalStatModifierItemSO.cs
@@ -1,9 +1,8 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "SO/Item/Artifact/StatConditionalStatModifierItem")]
-public class StatConditionalStatModifierItemSO : AttributedCallByEventItemSO
+[CreateAssetMenu(menuName = "SO/Item/Artifact/HealthConditionalStatModifierItem")]
+public class HealthConditionalStatModifierItemSO : AttributedCallByEventItemSO
 {
     private enum Condition
     {
@@ -12,23 +11,8 @@ public class StatConditionalStatModifierItemSO : AttributedCallByEventItemSO
         Greater = 1
     }
 
-    [Serializable]
-    private struct ConditionSlot
-    {
-        public StatType statType;
-        public Condition condition;
-        public float compareValue;
-
-        public bool IsCondition(float value) 
-        {
-            int compare = value.CompareTo(compareValue);
-            return compare == (int)condition;
-        }
-    }
-
-    protected override EventType CallingEventType => EventType.OnTurnEnded;
-
-    [SerializeField] List<ConditionSlot> conditions = new List<ConditionSlot>();
+    [SerializeField] Condition condition;
+    [SerializeField, Range(0f, 1f)] float compareValue;
     [SerializeField] List<StatModifierSlot> modifiers = new List<StatModifierSlot>();
 
     private bool actived = false;
@@ -51,6 +35,9 @@ public class StatConditionalStatModifierItemSO : AttributedCallByEventItemSO
             if(TryParseHandler<IStatModifierItemHandler>(OwnerHandler, out IStatModifierItemHandler statModifierHandler) == false)
                 return;
 
+            if (TryParseHandler<IHealthItemHandler>(OwnerHandler, out IHealthItemHandler healthItemHandler) == false)
+                return;
+
             StatSO stat = statModifierHandler.Stat;
             modifiers.ForEach(stat.RemoveModifier);
         }
@@ -63,16 +50,15 @@ public class StatConditionalStatModifierItemSO : AttributedCallByEventItemSO
         if(TryParseHandler<IStatModifierItemHandler>(OwnerHandler, out IStatModifierItemHandler statModifierHandler) == false)
             return;
 
-        StatSO stat = statModifierHandler.Stat;
+        if (TryParseHandler<IHealthItemHandler>(OwnerHandler, out IHealthItemHandler healthItemHandler) == false)
+            return;
 
-        bool condition = true;
-        for(int i = 0; i < conditions.Count; ++i)
-        {
-            ConditionSlot conditionSlot = conditions[i];
-            condition &= conditionSlot.IsCondition(stat[conditionSlot.statType]);
-            if(condition == false)
-                break;
-        }
+        StatSO stat = statModifierHandler.Stat;
+        IHealth health = healthItemHandler.Health;
+
+        float hpRatio = health.CurrentHp / (float)health.MaxHp;
+        int compare = hpRatio.CompareTo(compareValue);
+        bool condition = compare == (int)this.condition;
 
         if(actived)
         {
