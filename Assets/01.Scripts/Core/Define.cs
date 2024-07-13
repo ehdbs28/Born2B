79f5626f37dcb95590d5cf89870ac1f4de183554
ehdbs28 <doyun5373@naver.com>
2024-07-13 13:33:31 +0900

    feat: item holder 제작 함

--- Assets/01.Scripts/Core/Define.cs
+++ Assets/01.Scripts/Core/Define.cs
@@ -1,23 +1,26 @@
-using System;
+using UnityEngine;
 
 public static class UIDefine
 {
     public static string GetRarityKoreanText(ItemRarity rarity)
     {
-        switch (rarity)
+        return rarity switch
         {
-            case ItemRarity.None:
-                break;
-            case ItemRarity.Common:
-                break;
-            case ItemRarity.Rare:
-                break;
-            case ItemRarity.Epic:
-                break;
-            case ItemRarity.END:
-                break;
-
-        }
-            return "";
+            ItemRarity.Common => "커먼",
+            ItemRarity.Rare => "레어",
+            ItemRarity.Epic => "에픽",
+            _ => ""
+        };
+    }
+    
+    public static Color GetColorByRarity(ItemRarity rarity)
+    {
+        return rarity switch
+        {
+            ItemRarity.Common => new Color(0f, 157f / 255f, 0f, 1f),
+            ItemRarity.Rare => Color.blue,
+            ItemRarity.Epic => new Color(135f / 255f, 0f, 185 / 255f, 1f),
+            _ => Color.white
+        };
     }
 }
