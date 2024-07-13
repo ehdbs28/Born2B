af25df57125ebb43a43b39c3fb8bf7ed175c9507
SEH00N <06psh0501@gmail.com>
2024-07-13 23:41:15 +0900

    blink feedback

--- Assets/01.Scripts/System/Feedback/BlinkFeedback.cs
+++ Assets/01.Scripts/System/Feedback/BlinkFeedback.cs
@@ -6,30 +6,38 @@ public class BlinkFeedback : Feedback
     [SerializeField] Renderer targetRenderer = null;
     [SerializeField] Material blinkMaterial = null;
     [SerializeField] float delay = 0.15f;
+    [SerializeField] int blinkCount = 2;
 
+    private int counter = 0;
     private Material originMaterial = null;
 
     public override void Play(Vector3 playPos)
     {
+        counter = 0;
         originMaterial = targetRenderer.material;
-        targetRenderer.material = blinkMaterial;
-        StartSafeCoroutine("Blink", this.DelayCoroutine(delay, () => {
-            targetRenderer.material = originMaterial;
-            StartCoroutine(this.DelayCoroutine(delay, () => {
 
-            }));
-        }));
+        BlinkLoop();
+    }
+
+    private void BlinkLoop()
+    {
+        counter++;
+        SetBlink(blinkMaterial, () => {
+            SetBlink(originMaterial, counter >= blinkCount ? null : BlinkLoop);
+        });
     }
 
     private void SetBlink(Material materail, Action callback)
     {
         targetRenderer.material = materail;
-        StartCoroutine(this.DelayCoroutine(delay, callback));
+        if(callback != null)
+            StartSafeCoroutine("Blink", this.DelayCoroutine(delay, callback));
     }
 
     public override void Stop()
     {
         base.Stop();
         StopSafeCoroutine("Blink");
+        targetRenderer.material = originMaterial;
     }
 }
